03e2134e051980f7613a8e83d301fe17a5774e28
mushanga <cagdasozek@gmail.com>
2016-10-06 19:04:29 +0200

    Adding NotNull validation for search term

--- src/main/java/com/goeuro/service/SuggestionExportService.java
+++ src/main/java/com/goeuro/service/SuggestionExportService.java
@@ -9,10 +9,11 @@ import javax.validation.constraints.Pattern;
 @Validated
 public interface SuggestionExportService {
 
-    void searchAndExport(@Pattern(regexp = ".*\\S.*", message = "Search term can't be empty") String term,
+    void searchAndExport(@NotNull(message = "Search term can't be null")
+                         @Pattern(regexp = ".*\\S.*", message = "Search term can't be empty") String term,
                          @NotNull(message = "Output file name can't be null")
                          @Pattern(regexp = "[a-zA-Z0-9\\.\\-_]+",
-                                 message = "Output file name can contain only " +
+                                 message = "Output file name must exist and contain only " +
                                          "alphanumeric characters, '-', '_', and '.' ") String outputFilename);
 
 }
