a7519a1f34cf708083f66cb6290ee3bca0b41a85
mushanga <cagdasozek@gmail.com>
2016-10-07 11:58:05 +0200

    Replacing the hibernate validation with javax

--- src/main/java/com/goeuro/service/SuggestionService.java
+++ src/main/java/com/goeuro/service/SuggestionService.java
@@ -2,14 +2,16 @@ package com.goeuro.service;
 
 
 import com.goeuro.dto.SuggestionResponseDTO;
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Validated
 public interface SuggestionService {
 
-    List<SuggestionResponseDTO> search(@NotBlank String name);
+    List<SuggestionResponseDTO> search(@NotNull(message = "Search term can't be null")
+                                       @Pattern(regexp = ".*\\S.*", message = "Search term can't be empty") String name);
 
 }
